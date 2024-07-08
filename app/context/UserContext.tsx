211028bc6f4f68e2fb055f3a355783e1dfe5a6b7
Luiz Felipe Gois <luizfelipegois.contact@gmail.com>
2024-07-07 22:38:51 -0300

    feat: Constrói contexto global da aplicação para trabalhar com usuários.
    - Define a tipagem de dados para usuário.
    - Define um estado global para usar e alterar dados do usuário.

---
+++ app/context/UserContext.tsx
@@ -0,0 +1,40 @@
+"use client"
+
+import React, { createContext, useContext, useState, ReactNode } from "react";
+
+interface UserData {
+  _id: string;
+  username: string;
+  fullName: string;
+  email: string;
+  password: string;
+  phone: string;
+  dateOfBirth: string;
+}
+
+interface UserContextType {
+  user: UserData | null;
+  setUser: React.Dispatch<React.SetStateAction<UserData | null>>;
+}
+
+const UserContext = createContext<UserContextType | undefined>(undefined);
+
+export const UserProvider: React.FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  const [user, setUser] = useState<UserData | null>(null);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUserData = () => {
+  const context = useContext(UserContext);
+  if (!context) {
+    throw new Error("useUserData must be used within a UserProvider");
+  }
+  return context;
+};
